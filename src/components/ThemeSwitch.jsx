24992a4c6fe8ec9f9b9486ab00ffb115d2bb7911
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2022-11-15 23:55:20 -0300

    Add theme switch component

---
+++ src/components/ThemeSwitch.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { toggleTheme } from "../features/themeSlice";
+
+export default function ThemeSwitch() {
+  const theme = useSelector((state) => state.theme);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button type="text" onClick={() => dispatch(toggleTheme())}>
+        {theme.darkMode ? (
+          <MoonIcon className="h-8 w-8 text-gray-100" />
+        ) : (
+          <SunIcon className="h-8 w-8 text-gray-500" />
+        )}
+      </button>
+    </div>
+  );
+}

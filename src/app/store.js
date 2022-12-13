f632183fd13d0f2820bd49b6b3d6b191ab134f15
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2022-12-12 22:40:59 -0300

    Add reducers to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import themeReducer from "./../features/themeSlice";
+import userReducer from "./../features/user/userSlice";
+import loginReducer from "./../features/user/loginSlice";
+import noteReducer from "./../features/notes/notesSlice";
 
 export default configureStore({
   reducer: {
     theme: themeReducer,
-  },
+    login: loginReducer,
+    user: userReducer,
+    notes: noteReducer
+  }
 });
